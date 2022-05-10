a974a112b1617be2109e232ca7c62589c7c6af4e
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2022-05-10 12:19:14 +0300

    Worked on Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,10 @@
 import ServiceCard from './ServiceCard';
+import Carousel from 'react-bootstrap/Carousel';
+import './Carousel.css';
 
 export default function Gallery() {
 
-    {/*Gallery Logic*/}
+    {/*Gallery Logic*/ }
     const importAll = r => r.keys().map(r);
     const images = importAll(require.context('./gallery'));
 
@@ -18,6 +20,16 @@ export default function Gallery() {
                     ))}
                 </div>
             </div>
+            {/*Carousel with Images*/}
+            {/*
+            <Carousel fade>
+                {images.map(imageName => (
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={imageName.default} alt={imageName.default}/>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+            */}
         </div>
     );
 }
