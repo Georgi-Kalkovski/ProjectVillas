cac2a187d90b401721a2b755d456af7041beaa6c
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2022-08-16 13:40:29 +0300

    Update Booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -9,13 +9,12 @@ export default function Booking() {
 
                 {/*Google Calendar*/}
                 <iframe title="googleCalendar"
-                    src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FSofia&hl=bg&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=1&showTz=0&src=Z2VvcmdpLmRvZ2Fub3Y5ODdAZ21haWwuY29t&src=YmcuYnVsZ2FyaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043'
+                    src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FSofia&hl=bg&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=1&showTz=0&src=Z2VvcmdpLmRvZ2Fub3Y5ODdAZ21haWwuY29t&src=YmcuYnVsZ2FyaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000&color=%230B8043'
                     style={{ borderWidth: 0 }}
                     width='800'
                     height='600'
                     frameborder='0'
                     scrolling='no'>
-
                 </iframe>
             </div >
         </div >
