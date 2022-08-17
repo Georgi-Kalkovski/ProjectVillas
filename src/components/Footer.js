a7e1082bd26bab5e82c5a8efcbecbe46744e1b36
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2022-08-17 09:35:01 +0300

    Finishing touches on the Project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <footer id='footer' className='text-center' >
 
                 <div id='copyright' className='grow'>
-                    <a href='https://github.com/Georgi-Kalkovski'>Powered by Georgi Kalkovski</a>
+                    <a href='https://github.com/Georgi-Kalkovski'>Powered by <span style={{color:'#F8E73F'}}> Georgi Kalkovski</span></a>
                 </div>
 
             </footer>
